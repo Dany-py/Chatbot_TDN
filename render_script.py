666d1d39e24e5cc57c82f5ba76190fc0c0e24d71
Dany-py <danydegbe@gmail.com>
2025-10-09 22:02:51 +0000

    Add a script for reload render services

---
+++ render_script.py
@@ -0,0 +1,29 @@
+
+# Fonction pour maintenir les projets render disponible à tout moment.
+import requests
+import schedule
+import time
+
+render_links = [
+    "https://danymeteo.onrender.com/Meteo/",
+]
+
+def reload(urls):
+    results = {}
+    for url in urls:
+        try:
+            res = requests.get(url, timeout=10) 
+            results[url] = res.status_code == 200
+            print(f"Ping réussi pour {url}. Statut: {res.status_code}")
+        except requests.RequestException as e:
+            results[url] = False
+            print(f"Échec du ping pour {url}. Erreur: {e}")
+    return results
+
+
+# Appel récurant de la fonction reload
+schedule.every(10).minutes.do(reload, urls = render_links)
+
+while True:
+    schedule.run_pending() #Vérification et exécution de tâche.
+    time.sleep(1)
